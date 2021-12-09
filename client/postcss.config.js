26a8bad8d86b9dab1aef6da2bd20297c726fe6bc
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-12-09 19:21:06 +0900

    cssnano

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,13 +1,16 @@
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
-
+const cssnano = require('cssnano');
 module.exports = {
   plugins: [
+    cssnano({
+      preset: 'default',
+    }),
     postcssImport(),
     tailwindcss(),
-    postcssPresetEnv({
-      stage: 3,
-    }),
+    // postcssPresetEnv({
+    //   stage: 3,
+    // }),
   ],
 };
