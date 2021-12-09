729d70280dfefcc6da78f79996fe4efccff9e442
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-12-10 02:20:34 +0900

    remove moment

--- client/src/components/post/CommentItem/CommentItem.jsx
+++ client/src/components/post/CommentItem/CommentItem.jsx
@@ -1,7 +1,7 @@
-import moment from 'moment';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
 import { getProfileImagePath } from '../../../utils/get_path';
 
 /**
@@ -33,8 +33,8 @@ const CommentItem = ({ comment }) => {
           </p>
           <p className="text-gray-800 text-sm leading-relaxed">{comment.text}</p>
           <p className="text-gray-500 text-xs">
-            <time dateTime={moment(comment.createdAt).toISOString()}>
-              {moment(comment.createdAt).locale('ja').format('LL')}
+            <time dateTime={dayjs(comment.createdAt).toISOString()}>
+              {dayjs(comment.createdAt).locale('ja').format('YYYY年MMMMDD日')}
             </time>
           </p>
         </div>
